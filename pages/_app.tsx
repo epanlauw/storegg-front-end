59f4a4973658d2c43a12d72227aa26dcb3994c01
epanlauw <epanlauw6@gmail.com>
2022-06-19 11:39:33 +0700

    Little update in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -39,14 +39,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           href='https://unpkg.com/aos@2.3.1/dist/aos.css'
           rel='stylesheet'
         />
-
-        {/* Call Bootstrap */}
-        <Script
-          src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
-          integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
-          crossOrigin='anonymous'
-        />
       </Head>
+      {/* Call Bootstrap */}
+      <Script
+        src='https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js'
+        integrity='sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4'
+        crossOrigin='anonymous'
+      />
       <Component {...pageProps} />;
     </>
   );
