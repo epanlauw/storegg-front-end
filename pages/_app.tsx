665746472779004fcb786abbb4767662d5a176f5
epanlauw <epanlauw6@gmail.com>
2022-06-19 17:56:46 +0700

    Convert Transaction Detail Page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ import '../styles/sign-up-success.css';
 import '../styles/sign-up.css';
 import '../styles/utilities.css';
 import '../styles/transactions.css';
+import '../styles/transactions-detail.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
